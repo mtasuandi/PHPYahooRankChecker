d632c210ff58eaf6c8d1fc52b04b672ad4cb2bd4
M Teguh A Suandi <teguh.andro@gmail.com>
2013-06-27 19:35:07 +0700

    Fix Rank sorter

--- YahooRankChecker.class.php
+++ YahooRankChecker.class.php
@@ -2,6 +2,7 @@
 /**
 	@package	: Yahoo! RankChecker
 	@author		: M Teguh A Suandi
+	@email 		: teguh.andro@gmail.com
 	@license	: Creative Common License (http://creativecommons.org/licenses/by/3.0/)
 */
 if(!class_exists('YahooRankChecker'))
@@ -17,6 +18,7 @@ if(!class_exists('YahooRankChecker'))
 		
 		public function yahooFind($yahoodomain="", $keyword)
 		{
+			$i	= 1;
 			for($end=($this->end/$this->end);$end<=$this->end-9;$end+=10)
 			{
 				if(empty($yahoodomain))
@@ -27,23 +29,23 @@ if(!class_exists('YahooRankChecker'))
 				{
 					$domain = $yahoodomain.".";
 				}
+				
 				$keyword	= str_replace(" ","+",$keyword);
 				$url		= "http://".$domain."search.yahoo.com/search?p=".$keyword."&b=".$end."";
+				set_time_limit(120);
 				$data		= file_get_contents($url);
 				$dom 		= new DomDocument();
 				@$dom->loadHTML($data);
 				$finder 	= new DomXPath($dom);
 				$classname	= "url";
 				$nodes 		= $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-				$tmp_dom 	= new DOMDocument(); 
-				$i 			= 1;
+				$tmp_dom 	= new DOMDocument();
 				
 				foreach($nodes as $node) 
 				{
 					$node 		= $tmp_dom->appendChild($tmp_dom->importNode($node,true));
 					$site 		= $node->nodeValue;
 					$rank		= $i++;
-					
 					$result[]	= array("link" => $site, "rank" => $rank);
 				}
 			}
